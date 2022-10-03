d5e48c8f54c4a5c83afaa1fd196b4ff0d477fbd6
tvien <tonyxvien@gmail.com>
2022-10-02 23:53:33 -0400

    added basic dicom image loading functionality

--- src/app/dicom-viewer/dicom-viewer.component.ts
+++ src/app/dicom-viewer/dicom-viewer.component.ts
@@ -1,13 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import { Base64Service } from '../util/base64.service';
 import { DicomViewerService } from './dicom-viewer.service';
+import DCM_BASE64 from './example-dicom-base64';
 
 @Component({
   selector: 'app-dicom-viewer',
   templateUrl: './dicom-viewer.component.html',
-  styleUrls: ['./dicom-viewer.component.scss'],
 })
-export class DicomViewerComponent implements OnInit {
-  constructor(private dvs: DicomViewerService) {}
+export class DicomViewerComponent implements AfterViewInit {
+  @ViewChild('dicomImageContainer') dicomImageContainer?: ElementRef;
 
-  ngOnInit(): void {}
+  constructor(
+    private dvService: DicomViewerService,
+    private base64Service: Base64Service
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.loadDicom();
+  }
+
+  loadDicom(): void {
+    alert('loading dicom file!');
+
+    let blob = this.base64Service.convertBase64toBlob(DCM_BASE64);
+
+    var dcmFile = new File([blob], 'dcm');
+
+    // TODO: determine what type of dicom - image or encapsulated pdf? For now, we assume an image
+    // NOTE: we can possibly use the dicom-parser library to parse and view SOP ID to figure this out
+
+    if (this.dicomImageContainer) {
+      this.dvService
+        .loadDicomImage(dcmFile, this.dicomImageContainer.nativeElement)
+        .then((res) => console.log(res));
+    }
+  }
 }
--- src/app/dicom-viewer/dicom-viewer.service.ts
+++ src/app/dicom-viewer/dicom-viewer.service.ts
@@ -1,18 +1,51 @@
-import { Injectable } from '@angular/core';
+import { ElementRef, Injectable } from '@angular/core';
 
+import { Image } from 'cornerstone-core';
 import * as cs from 'cornerstone-core';
 import * as dp from 'dicom-parser';
 
-const wadoImageLoader = require('cornerstone-wado-image-loader');
+const imageLoader = require('cornerstone-wado-image-loader');
 
 @Injectable({
   providedIn: 'root',
 })
 export class DicomViewerService {
-  constructor() {
-    console.log(cs);
-    console.log(dp);
+  private cornerstone: any;
+  private dicomParser: any;
+  private imageLoader: any;
 
-    console.log(wadoImageLoader);
+  constructor() {
+    this.cornerstone = cs;
+    this.dicomParser = dp;
+    this.imageLoader = imageLoader;
+
+    this.configureImageLoader();
+  }
+
+  configureImageLoader() {
+    // set cornerstone instance to the imageLoader
+    this.imageLoader.external.cornerstone = this.cornerstone;
+    this.imageLoader.external.dicomParser = this.dicomParser;
+  }
+
+  // this UI is only built for a single file so just dump the first one
+  loadDicomImage(dcmFile: File, element: HTMLElement): Promise<Image> {
+    // TODO: these added files will probably need to be removed when a new file is requested
+    const imageId = this.imageLoader.wadouri.fileManager.add(dcmFile);
+
+    this.cornerstone.enable(element);
+
+    return this.cornerstone.loadImage(imageId).then((img: Image) => {
+      // load the dicom image into the given container element
+      const viewport = this.cornerstone.getDefaultViewportForImage(
+        element,
+        img
+      );
+
+      this.cornerstone.displayImage(element, img, viewport);
+
+      // pass along the cornerstone Image object for further optional processing
+      return img;
+    });
   }
 }
